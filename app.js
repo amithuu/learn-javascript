a8894b6a6c414db4de8060c9d894d14b73ff5865
amithuu <amithkulkarni98@gmail.com>
2024-05-17 17:26:20 +0530

    feat: import javascript

---
+++ app.js
@@ -0,0 +1,9 @@
+// // input
+// var username = prompt('enter your name');
+
+// // process 
+// var WelcomeMessage = 'Welcome to my new learning '+ username
+
+// // output
+// alert(WelcomeMessage)
+
