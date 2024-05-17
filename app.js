9307a457b380bc2d7a4d486a1b1cd74f883bd025
amithuu <amithkulkarni98@gmail.com>
2024-05-17 18:03:26 +0530

    feat: added text and button with js event listener

--- app.js
+++ app.js
@@ -7,3 +7,14 @@
 // // output
 // alert(WelcomeMessage)
 
+var btnTranslate = document.querySelector('#btn-translate');
+var txtInput = document.querySelector('#txt-input');
+
+
+function clickEventHandler(){
+    console.log('clicked!!')
+    console.log('input', txtInput.value);
+}
+
+btnTranslate.addEventListener('click', clickEventHandler)
+
