c6196bb66e81019db97de4afdc7bbfa949a39e3c
amithuu <amithkulkarni98@gmail.com>
2024-05-17 18:18:54 +0530

    feat: taken input from user and shown in div

--- app.js
+++ app.js
@@ -9,11 +9,12 @@
 
 var btnTranslate = document.querySelector('#btn-translate');
 var txtInput = document.querySelector('#txt-input');
+var outputDiv = document.querySelector('#output');
+
 
 
 function clickEventHandler(){
-    console.log('clicked!!')
-    console.log('input', txtInput.value);
+    outputDiv.innerText = txtInput.value;
 }
 
 btnTranslate.addEventListener('click', clickEventHandler)
